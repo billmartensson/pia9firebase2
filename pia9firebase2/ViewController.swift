7cc80b35930664a973cd874e978bf6501cff2e29
Bill Mårtensson <bill@magictechnology.se>
2020-09-21 11:21:19 +0200

    Start

--- pia9firebase2/ViewController.swift
+++ pia9firebase2/ViewController.swift
@@ -6,12 +6,46 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
+    var ref: DatabaseReference!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        ref = Database.database().reference()
+        
+        ref.child("products").child("firstproduct").child("productname").setValue("Skruvmejsel")
+        ref.child("products").child("firstproduct").child("color").setValue("Blå")
+        ref.child("products/firstproduct/amount").setValue(7)
+
+        let prodName = "secondproduct"
+        ref.child("products/\(prodName)/productname").setValue("Hammare")
+        
+        print("Vår andra produkt är \(prodName) och den är jättebra")
+        
+        
+        print("Nu ska vi hämta data")
+        ref.child("products/firstproduct").observeSingleEvent(of: .value, with: { (snapshot) in
+          
+            let allTheProductInfo = snapshot.value as! NSDictionary
+            
+            let pname = allTheProductInfo["productname"] as! String
+            let pcolor = allTheProductInfo["color"] as! String
+            
+            
+            print("Produkten heter \(pname)")
+            print("Färgen är \(pcolor)")
+
+            
+          }) { (error) in
+            print(error.localizedDescription)
+        }
+        print("Nu har vi hämtat data")
+        
     }
 
 
